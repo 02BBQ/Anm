c8792fe43ed45c5dcf5c999ffb736e3f0c53a3ea
02BBQ <bbqcute63@gmail.com>
2025-10-07 03:43:00 +0900

    feat: Fallen Kick VFX and add TwinSlashes skill
    Improved the Fallen Kick visual effects by adding new sound cues, raycast-based slam effects, and crater impact logic. Introduced the TwinSlashes skill script for the FallenOne class, which plays an animation when cast if conditions are met.

--- src/ReplicatedStorage/Shared/VFXHandler/Fallen/Kick.lua
+++ src/ReplicatedStorage/Shared/VFXHandler/Fallen/Kick.lua
@@ -29,6 +29,8 @@ VFX.start = function(Data)
 	if not Animation then return end;
 
 	local jumpMaid = Auxiliary.Maid.new();
+
+	Sound.Spawn("Fallen/Sukuna Laugh", Root, .9);
 	
 	local bp: BodyPosition = Auxiliary.Shared.CreatePosition(Root, {MaxForce = Vector3.one * 1e6});
 	jumpMaid:AddTask(bp);
@@ -48,13 +50,20 @@ VFX.start = function(Data)
 	smoke.Parent = FXParent;
 	Auxiliary.Shared.PlayAttachment(smoke);
 
+	task.delay(5,function()
+		jumpMaid:Destroy();
+	end)
+
 	Auxiliary.Shared.WaitForMarker(Animation, "kick")
+	local origin = Root.CFrame;
 	bp:Destroy();
+
+	Sound.Spawn("Fallen/sukuna_slash_single", Root, 3)
 	
 	local meshVFX = Auxiliary.Maid.new();
 	
-	local part = Assets.nothing:Clone();
-	part.CFrame = Root.CFrame * CFrame.new(0,0,-3) * CFrame.Angles(0,0,math.pi/2);
+	local part = Auxiliary.Shared.BindFX(meshVFX, Assets.nothing);
+	part.CFrame = Root.CFrame * CFrame.new(0,0,-3) * CFrame.Angles(0,0,-math.pi/3);
 	part.Parent = workspace.World.Debris;
 	
 	local meshes2: Model = Auxiliary.Shared.BindFX(meshVFX, Auxiliary.MeshEmitter.StartMeshEmitter(
@@ -72,6 +81,59 @@ VFX.start = function(Data)
 	task.delay(5,function()
 		meshVFX:Destroy();
 	end)
+
+	task.wait(0.5);
+
+	local ray: RaycastResult = workspace:Raycast(origin.Position, origin.LookVector * 1000, Auxiliary.Shared.RayParams.Map);
+
+	if not ray then return end;
+
+	local slamVFX = Auxiliary.Maid.new();
+
+	local Dark = Assets.Dark2:Clone();
+	Dark.Parent = game.Lighting;
+
+	local part = Auxiliary.Shared.BindFX(slamVFX, Assets.slammin);
+	part:PivotTo(CFrame.new(ray.Position, ray.Normal + ray.Position) * CFrame.Angles(0,0,-math.pi/3));
+	part.Parent = FXParent;
+	local meshes2: Model = Auxiliary.Shared.BindFX(slamVFX, Auxiliary.MeshEmitter.StartMeshEmitter(
+		part,
+		Assets.DisMesh,
+		CFrame.Angles(-math.pi/2,0,0) * CFrame.new(0,0,1),
+		0.11
+	));
+
+	local meshes2: Model = Auxiliary.Shared.BindFX(meshVFX, Auxiliary.MeshEmitter.StartMeshEmitter(
+		part,
+		Assets.Disdzxmantle,
+		CFrame.Angles(-math.pi/2,0,0) * CFrame.new(0,0,1)
+	));
+	
+	task.wait(0.11);
+	Dark:Destroy();
+	
+	Auxiliary.Shared.PlayAttachment(part);
+	Sound.Spawn("Fallen/sukuna_WCS_hit_sfx", Root, 6)
+
+	task.delay(5,function()
+		slamVFX:Destroy();
+	end)
+
+
+	Auxiliary.Crater:Impact({
+		Start = origin.Position,
+		End = origin.LookVector,
+		Seed = tick(),             -- 랜덤 시드
+		NoSound = false,           -- 사운드 허용
+		NoCrater = false,          -- 크레이터 생성
+		Amount = 7;
+		NoSmoke = true,            -- 연기는 주석처리되어 사용 안됨
+		NoDebris = true,          -- 바위 파편 허용
+		amount = 7,                -- 지면 파편 개수
+		sizemult = 2.5,              -- 파편 크기 배수
+		size = 5,                   -- 크레이터 크기
+		DespawnTime = 5
+	})
 end
 
 return VFX;
---
+++ src/ServerScriptService/Server/Skills/Class/FallenOne/TwinSlashes.lua
@@ -0,0 +1,21 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local ServerScriptService = game:GetService("ServerScriptService");
+
+local Shared = ReplicatedStorage.Shared;
+local SharedComponents = Shared.Services;
+local Auxiliary = require(Shared.Utility.Auxiliary);
+
+local Spell = require(ServerScriptService.Server.Skills.Spell):Extend();
+Spell.Name = script.Name;
+Spell.CastSign = 0;
+
+function Spell:OnCast(Entity, Args)
+	if not Args["held"] then return end;
+	if not Entity.Combat:CanUse() then return end;
+
+	local Start : AnimationTrack = Entity.Animator:Fetch("Fallen/double");
+	Start:Play();
+end;
+
+
+return Spell;
