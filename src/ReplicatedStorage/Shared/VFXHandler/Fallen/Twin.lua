ef2252f96d3d2f51af6281720016380553da8d1d
02BBQ <bbqcute63@gmail.com>
2025-10-08 05:59:26 +0900

    chore: twin more effects!!

--- src/ReplicatedStorage/Shared/VFXHandler/Fallen/Twin.lua
+++ src/ReplicatedStorage/Shared/VFXHandler/Fallen/Twin.lua
@@ -39,18 +39,61 @@ VFX.start = function(Data)
 	fx.Parent = FXParent;
 	Auxiliary.Shared.PlayAttachment(fx);
 
+	local rootFX: Model = Auxiliary.Shared.BindFX(VFXMaid, Assets.CharacterFX);
+	rootFX.CFrame = Root.CFrame;
+	rootFX.Weld.Part0 = Root;
+	rootFX.Parent = FXParent;
+	
+	Auxiliary.Shared.PlayAttachment(rootFX.Charge);
+	
+	Auxiliary.Shared.WaitForMarker(Animation, "vfx1");
+
+	-- Auxiliary.Shared.PlayAttachment(rootFX.ChargeFinish);
+
+	local claw2: Model = Auxiliary.Shared.BindFX(VFXMaid, Assets.Slash);
+	claw2.CFrame = Root.CFrame;
+	claw2.Weld.Part0 = Root;
+	claw2.Weld.C0 *= CFrame.Angles(0,math.rad(-110),0);
+	claw2.Parent = FXParent;
+
+	for _, beam in pairs(claw2:GetDescendants()) do
+		if beam:IsA("Beam") then
+			local Tween = TweenService:Create(beam, TweenInfo.new(0.3, Enum.EasingStyle.Exponential, Enum.EasingDirection.In), {
+				Width0 = 0;
+				Width1 = 0;
+			});
+			Tween:Play();
+		end
+	end
+
+	task.delay(0.3, function()
+		claw2:Destroy();
+	end)
+
+	local c1 c1 = RunService.RenderStepped:Connect(function(deltaTime)
+		if not claw2 or not claw2:IsDescendantOf(workspace) then c1:Disconnect(); return; end;
+		claw2.Weld.C0 *= CFrame.Angles(0,math.pi*4*deltaTime,0);
+	end)
+
+	task.delay(7, function()
+		VFXMaid:Destroy();
+	end)
+
 	Auxiliary.Shared.WaitForMarker(Animation, "first");
+
+	Auxiliary.Shared.PlayAttachment(rootFX.RootEmit);
+
+	local fx = Auxiliary.Shared.BindFX(VFXMaid, Assets.Dash);
+	fx.CFrame = Root.CFrame;
+	fx.Parent = FXParent;
+	Auxiliary.Shared.PlayAttachment(fx);
 	
 	local fx = Auxiliary.Shared.BindFX(VFXMaid, Assets.FirstTwin);
 	fx:PivotTo(Root.CFrame);
 	fx.Parent = FXParent;
 	Auxiliary.Shared.PlayAttachment(fx);
 
-	task.delay(10, function()
-		VFXMaid:Destroy();
-	end)
-
-	local Tween = TweenService:Create(fx.SpaceDistortion, TweenInfo.new(0.5, Enum.EasingStyle.Cubic), {CFrame = fx.SpaceDistortion.CFrame * CFrame.new(0,0,-15);});
+	local Tween = TweenService:Create(fx.SpaceDistortion, TweenInfo.new(0.5, Enum.EasingStyle.Cubic), {CFrame = fx.SpaceDistortion.CFrame * CFrame.new(0,0,-20);});
 	Tween:Play();
 	
 	local speed = Instance.new("NumberValue");
@@ -72,14 +115,51 @@ VFX.start = function(Data)
 		firstMaid:Destroy();
 	end)
 
+	Auxiliary.Shared.WaitForMarker(Animation, "firstend");
+
+	Auxiliary.Shared.PlayAttachment(rootFX.Finish);
+
 	Auxiliary.Shared.WaitForMarker(Animation, "second");
+
+	Auxiliary.Shared.PlayAttachment(rootFX.RootEmit);
+
+	local fx = Auxiliary.Shared.BindFX(VFXMaid, Assets.Dash);
+	fx.CFrame = Root.CFrame;
+	fx.Parent = FXParent;
+	Auxiliary.Shared.PlayAttachment(fx);
+
+	local claw2: Model = Auxiliary.Shared.BindFX(VFXMaid, Assets.Slash2);
+	claw2.CFrame = Root.CFrame;
+	claw2.Weld.Part0 = Root;
+	claw2.Weld.C0 *= CFrame.Angles(0,math.rad(-150),0);
+	claw2.Parent = FXParent;
+
+	for _, beam in pairs(claw2:GetDescendants()) do
+		if beam:IsA("Beam") then
+			local Tween = TweenService:Create(beam, TweenInfo.new(0.2, Enum.EasingStyle.Exponential, Enum.EasingDirection.In), {
+				Width0 = 0;
+				Width1 = 0;
+			});
+			Tween:Play();
+		end
+	end
+
+	task.delay(0.3, function()
+		claw2:Destroy();
+	end)
+
+	local c1 c1 = RunService.RenderStepped:Connect(function(deltaTime)
+		if not claw2 or not claw2:IsDescendantOf(workspace) then c1:Disconnect(); return; end;
+		claw2.Weld.C0 *= CFrame.Angles(0,math.pi*7*deltaTime,0);
+	end)
+
 
 	local fx = Auxiliary.Shared.BindFX(VFXMaid, Assets.SecondTwin);
 	fx:PivotTo(Root.CFrame);
 	fx.Parent = FXParent;
 	Auxiliary.Shared.PlayAttachment(fx);
 
-	local Tween = TweenService:Create(fx.SpaceDistortion, TweenInfo.new(0.5, Enum.EasingStyle.Cubic), {CFrame = fx.SpaceDistortion.CFrame * CFrame.new(0,0,-15);});
+	local Tween = TweenService:Create(fx.SpaceDistortion, TweenInfo.new(0.5, Enum.EasingStyle.Cubic), {CFrame = fx.SpaceDistortion.CFrame * CFrame.new(0,0,-20);});
 	Tween:Play();
 
 	firstMaid:Destroy();
